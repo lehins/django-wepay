5a49c9a99287cd3be9c2cd9c981a91e4f0cb75dd
Alexey Kuleshevich <lehins@yandex.ru>
2015-01-06 19:09:07 -0700

    made it compatible with Django 1.7

--- djwepay/fields.py
+++ djwepay/fields.py
@@ -9,10 +9,18 @@ try:
 except ImportError: pass
 
 class MoneyField(models.DecimalField):
+
     def __init__(self, *args, **kwargs):
-        if not 'decimal_places' in kwargs:
-            kwargs['decimal_places'] = 2
-        if not 'max_digits' in kwargs:
-            kwargs['max_digits'] = 11 # ~1 billion USD, should be enough for now
+        kwargs.setdefault('decimal_places', 2)
+        kwargs.setdefault('max_digits', 11) # ~1 billion USD, should be enough for now
         super(MoneyField, self).__init__(*args, **kwargs)
 
+    def deconstruct(self):
+        try:
+            name, path, args, kwargs = super(MoneyField, self).deconstruct()
+            if self.decimal_places != 2:
+                kwargs['decimal_places'] = self.decimal_places
+            if self.max_digits != 11:
+                kwargs['max_digits'] = self.max_digits
+            return name, path, args, kwargs
+        except AttributeError: pass
